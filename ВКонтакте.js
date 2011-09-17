4e486b9245e74a53a47be2b32edb14d8612be575
Marc Hoyois <marc.hoyois@gmail.com>
2011-09-16 19:47:15 -0500

    SDfsdfsd

--- ВКонтакте.js
+++ ВКонтакте.js
@@ -1,20 +1,17 @@
-// ВКонтакте killer (2011-08-14)
-// by Marc Hoyois
+// ВКонтакте killer (2011-09-16)
 
-var killer = new Object();
-addKiller("ВКонтакте", killer);
+addKiller("ВКонтакте", {
 
-killer.canKill = function(data) {
-	if(data.plugin !== "Flash") return false;
+"canKill": function(data) {
 	return data.src.indexOf("vkontakte.ru/swf/") !== -1;
-};
+},
 
-killer.process = function(data, callback) {
-	var flashvars = parseFlashVariables(data.params);
+"process": function(data, callback) {
+	var flashvars = parseFlashVariables(data.params.flashvars);
 	
 	var posterURL = decodeURIComponent(flashvars.thumb);
 	var title = unescapeHTML(decodeURIComponent(flashvars.md_title));
-	var sources = new Array();
+	var sources = [];
 	
 	var host = decodeURIComponent(flashvars.host);
 	if(!/^http:/.test(host)) host = "http://" + host;
@@ -22,21 +19,22 @@ killer.process = function(data, callback) {
 	
 	if(flashvars.uid && flashvars.uid !== "0") {
 		var url = host + "u" + decodeURIComponent(flashvars.uid) + "/video/" + decodeURIComponent(flashvars.vtag) + ".";
-		if(hd >= 2) sources.push({"url": url + "480.mp4", "format": "480p MP4", "height": 480, "isNative": true, "mediaType": "video"});
-		if(hd >= 1) sources.push({"url": url + "360.mp4", "format": "360p MP4", "height": 360, "isNative": true, "mediaType": "video"});
+		if(hd >= 3) sources.push({"url": url + "720.mp4", "format": "720p MP4", "height": 720, "isNative": true,});
+		if(hd >= 2) sources.push({"url": url + "480.mp4", "format": "480p MP4", "height": 480, "isNative": true,});
+		if(hd >= 1) sources.push({"url": url + "360.mp4", "format": "360p MP4", "height": 360, "isNative": true});
 		if(flashvars.no_flv === "1") {
-			sources.push({"url": url + "240.mp4", "format": "240p MP4", "height": 240, "isNative": true, "mediaType": "video"});
+			sources.push({"url": url + "240.mp4", "format": "240p MP4", "height": 240, "isNative": true});
 		} else {
-			sources.push({"url": url + "flv", "format": "240p FLV", "height": 240, "isNative": false, "mediaType": "video"});
+			sources.push({"url": url + "flv", "format": "240p FLV", "height": 240, "isNative": false});
 		}
-		
 	} else {
 		var url = host + "/assets/video/" + decodeURIComponent(flashvars.vtag) + decodeURIComponent(flashvars.vkid) + ".vk.flv";
-		sources.push({"url": url, "format": "240p FLV", "height": 240, "isNative": false, "mediaType": "video"});
+		sources.push({"url": url, "format": "240p FLV", "height": 240, "isNative": false});
 	}
 	
-	var mediaData = {
+	callback({
 		"playlist": [{"poster": posterURL, "title": title, "sources": sources}]
-	};
-	callback(mediaData);
-};
+	});
+}
+
+});
