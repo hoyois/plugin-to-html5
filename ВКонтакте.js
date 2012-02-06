c846d8f7d4b70be7a60e24b5d87e44243a5dcc38
Marc Hoyois <marc.hoyois@gmail.com>
2012-02-05 18:10:15 -0600

    Fix vkontakte killer

--- ВКонтакте.js
+++ ВКонтакте.js
@@ -1,4 +1,4 @@
-// ВКонтакте killer (2011-09-16)
+// ВКонтакте killer (2012-02-05)
 
 addKiller("ВКонтакте", {
 
@@ -14,11 +14,10 @@ addKiller("ВКонтакте", {
 	var sources = [];
 	
 	var host = decodeURIComponent(flashvars.host);
-	if(!/^http:/.test(host)) host = "http://" + host;
 	var hd = parseInt(flashvars.hd);
 	
 	if(flashvars.uid && flashvars.uid !== "0") {
-		var url = host + "u" + decodeURIComponent(flashvars.uid) + "/video/" + decodeURIComponent(flashvars.vtag) + ".";
+		var url = "http://cs" + host + ".vk.com/u" + decodeURIComponent(flashvars.uid) + "/video/" + decodeURIComponent(flashvars.vtag) + ".";
 		if(hd >= 3) sources.push({"url": url + "720.mp4", "format": "720p MP4", "height": 720, "isNative": true,});
 		if(hd >= 2) sources.push({"url": url + "480.mp4", "format": "480p MP4", "height": 480, "isNative": true,});
 		if(hd >= 1) sources.push({"url": url + "360.mp4", "format": "360p MP4", "height": 360, "isNative": true});
@@ -28,6 +27,7 @@ addKiller("ВКонтакте", {
 			sources.push({"url": url + "flv", "format": "240p FLV", "height": 240, "isNative": false});
 		}
 	} else {
+		if(!/^http:/.test(host)) host = "http://" + host;
 		var url = host + "/assets/video/" + decodeURIComponent(flashvars.vtag) + decodeURIComponent(flashvars.vkid) + ".vk.flv";
 		sources.push({"url": url, "format": "240p FLV", "height": 240, "isNative": false});
 	}
