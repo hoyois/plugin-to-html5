78374a7ee9a2b5dc9fa5a9a6078ee1eaee6cdb86
Marc Hoyois <marchoyois@Marcs-MacBook-Pro.local>
2013-02-12 12:54:29 -0600

    updated Dailymotion killer for site changes

--- Dailymotion.js
+++ Dailymotion.js
@@ -22,17 +22,23 @@ addKiller("Dailymotion", {
 	var media = base.sequenceList.filter(name("main"))[0].layerList.filter(name("video"))[0].param;
 	var params = base.sequenceList.filter(name("reporting"))[0].layerList.filter(name("reporting"))[0].param.extraParams;
 	
-	var sources = [];
-	if(media.hd1080URL) sources.push({"url": media.hd1080URL.replace(/\\\//g,"/"), "format": "1080p MP4", "height": 1080, "isNative": true});
-	if(media.hd720URL) sources.push({"url": media.hd720URL.replace(/\\\//g,"/"), "format": "720p MP4", "height": 720, "isNative": true});
-	if(media.hqURL) sources.push({"url": media.hqURL.replace(/\\\//g,"/"), "format": "480p MP4", "height": 480, "isNative": true});
-	if(media.sdURL) sources.push({"url": media.sdURL.replace(/\\\//g,"/"), "format": "360p MP4", "height": 360, "isNative": true});
-	if(media.ldURL) sources.push({"url": media.ldURL.replace(/\\\//g,"/"), "format": "240p MP4", "height": 240, "isNative": true});
-	
 	var title = unescape(params.videoTitle.replace(/\+/g, " ").replace(/\\u/g, "%u").replace(/\\["'\/\\]/g, function(s){return s.charAt(1);})); // sic
 	var poster = params.videoPreviewURL.replace(/\\\//g,"/");
 	
-	callback({"playlist": [{"title": title, "poster": poster, "sources": sources}]});
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", media.autoURL.replace(/\\\//g,"/"), true);
+	xhr.addEventListener("load", function () {
+		var config = JSON.parse(xhr.responseText);
+		
+		var sources = [];
+		for(var i = config.alternates.length - 1; i >= 0; i--) {
+			var source = config.alternates[i];
+			sources.push({"url": source.template.replace(/mnft$/, "mp4"), "format": source.name + "p MP4", "height": parseInt(source.name), "isNative": true});
+		}
+		
+		callback({"playlist": [{"title": title, "poster": poster, "sources": sources}]});
+	}, false);
+	xhr.send(null);
 },
 
 "processVideoID": function(videoID, callback) {
