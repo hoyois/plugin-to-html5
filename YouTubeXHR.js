2e326e5d3368485e6b58f38bda1f62f02f195577
Marc Hoyois <marc.hoyois@gmail.com>
2014-06-24 15:37:34 +0200

    replace dbl quotes in YouTube video titles

--- YouTubeXHR.js
+++ YouTubeXHR.js
@@ -10,7 +10,7 @@ if(window.safari) {
 
 addKiller("YouTube", {
 
-"playlistFilter": /^UL|^PL|^SP|^AL/,
+"playlistFilter": /^UL|^PL|^FL|^SP|^AL/,
 
 "canKill": function(data) {
 	if(/^https?:\/\/s\.ytimg\.com\//.test(data.src)) return true;
@@ -100,7 +100,7 @@ addKiller("YouTube", {
 				source = {"format": "240p FLV", "height": 240, "isNative": false};
 			} else continue;
 			
-			source.url = decodeURIComponent(fmt.url) + "&title=" + flashvars.title + "%20%5B" + source.height + "p%5D";
+			source.url = decodeURIComponent(fmt.url) + "&title=" + flashvars.title.replace(/%22/g, "	%27") + "%20%5B" + source.height + "p%5D";
 			if(fmt.sig) source.url += "&signature=" + fmt.sig;
 			else if(fmt.s) source.url += "&signature=" + this.decodeSignature(fmt.s);
 			sources.push(source);
