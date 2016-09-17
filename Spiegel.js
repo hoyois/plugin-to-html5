dcd94b3a5010f2ee58a7614d9b04a134ccffe5f6
Marc Hoyois <marc.hoyois@gmail.com>
2016-09-16 23:07:58 -0400

    Spiegel.js fix from psandtner

--- Spiegel.js
+++ Spiegel.js
@@ -1,43 +1,88 @@
-if(window.safari) {
+if (window.safari) {
 	// SPIEGEL.DE HACKS for ClickToPlugin
 	safari.extension.addContentStyleSheet("div.jw-controls.jw-reset {display: none !important;}", ["http://*.spiegel.de/*"], []);
+	
+	var injectedScript = `
+		// This script tries to get the videoID out of the DOM
+		function respondToMessage(theMessageEvent) {
+			if(theMessageEvent.name === "getCurrentVideoID") {
+				var videoID;
+				var matchingListElement = document.querySelector("#js-video-slider .bx-wrapper .bx-viewport .bxslider .item_active");
+				if (matchingListElement) {
+					var dataVideoNode = matchingListElement.getAttributeNode("data-video");
+					videoID = dataVideoNode.value;
+				}
+	    	    safari.self.tab.dispatchMessage("pushCurrentVideoID", videoID);
+	    	}
+		}
+		safari.self.addEventListener("message", respondToMessage, false);
+	`;
+
+	safari.extension.addContentScript(injectedScript, ["http://*.spiegel.de/*"], [], true);
+
 }
 
 
 addKiller("Spiegel", {
-	
+
+
 "canKill": function(data) {
-	return (data.baseURL == "http://www.spiegel.de/video/") || 
-		(/^http:\/\/www\.spiegel\.de\/static\/flash/.test(data.src));
+	return data.baseURL.startsWith("http://www.spiegel.de/");
 },
+
 
 "process": function(data, callback) {
-	var _this = this;
-	if (data.baseURL == "http://www.spiegel.de/video/") {
-		var xhr = new XMLHttpRequest();
-		xhr.open("GET", data.baseURL, true);
-		xhr.addEventListener("load", function() {
-			var videoID = xhr.responseText.replace(/[\s\S]*<div id="spPanoPlayer(\d+)"[\s\S]*/g, "$1")
-			if(!isNaN(videoID)) {
-				_this.createCallback(videoID, callback);
-			} 
-		}, false);
-		xhr.send(null);
+	// Simple check if videoID is probably somewhere
+	if (data.location.indexOf("-video-") !== -1) {
+		var videoID = data.location.replace(/.*-video-(\d+).*.html/g, "$1");
+		this.processVideoIDAndCreateCallback(videoID, callback);
 	}
 	else {
-		var videoID = data.baseURL.replace(/.*-video-(\d+).*.html/g, "$1");
-		if(!isNaN(videoID)) {
-			_this.createCallback(videoID, callback);
-		} 
+		// Only in the following case it makes sense to install the eventListener:
+		if (data.src === "http://www.spiegel.de/static/flash/flashvideo/jwplayer7.flash.swf") {
+			var _this = this;
+			
+			var myMessageEventListener = function (theMessageEvent) {
+   			if(theMessageEvent.name === "pushCurrentVideoID") {
+   				// remove us:
+   				safari.application.removeEventListener(myMessageEventListener);
+			    var videoID = theMessageEvent.message;
+			    if(isNaN(videoID)) {
+					return;
+				}
+					_this.processVideoIDAndCreateCallback.call(_this, videoID, callback);
+			    }
+			}
+			
+			safari.application.addEventListener("message", myMessageEventListener, false);	
+			safari.application.activeBrowserWindow.activeTab.page.dispatchMessage("getCurrentVideoID", "data");	
+		}
 	}
+	
 },
 
 
-"createCallback": function(videoID, callback) {
+"processVideoIDAndCreateCallback": function(videoID, callback) {
+	var _this = this;
+	var xhr = new XMLHttpRequest();
+	var jsonInfoUrl = "http://www.spiegel.de/video/video-" + videoID + ".json";
+	xhr.open("GET", jsonInfoUrl, true);
+	xhr.addEventListener("load", function() {
+		var data = JSON.parse(xhr.responseText);
+		if(data) {
+			_this.createCallback(data, callback);
+		} 
+	}, false);
+	xhr.send(null);
+},
+
+
+"createCallback": function(data, callback) {
+	var videoURL = data.cdnhost + data.binaryfilename;
 	callback({
 			"playlist": [{
 				"sources": [{
-					"url": "http://www.spiegel.de/video/media/video-" + videoID + ".html",
+					"url": videoURL,
 					"isNative": true
 				}]
 			}]
